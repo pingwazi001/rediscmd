aa25fbb0a79e018eb4c2dbe43a42af43d37a3eb1
pwz <273206491@qq.com>
2021-08-03 19:47:53 +0800

    1、优化性能、支持多样化配置文件

--- src/util/command.go
+++ src/util/command.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/modood/table"
 )
@@ -59,23 +61,34 @@ func ReadConsoleLineStr() string {
 func FuncOptionMsg() {
 	msg := []KeyValueItem{
 		{"1", "加载数据库列表"},
-		{"2", "模糊查询指定数据库的缓存key"},
-		{"3", "查询对应key的值"},
-		{"4", "删除对应key的值"},
-		{"5", "设置对应key的值"},
-		{"6", "重新配置redis连接信息"},
-		{"7", "退出程序"},
+		{"2", "模糊查询缓存key"},
+		{"3", "查询模糊key的值"},
+		{"4", "删除模糊key的值"},
+		{"5", "设置精确key的值"},
+		{"6", fmt.Sprintf("重新配置%s文件内容", confName)},
+		{"7", "切换配置文件"},
+		{"8", "触发刷新本地缓存Key集合"},
+		{"9", "新增配置文件"},
+		{"10", "退出程序"},
 	}
 	t := table.AsciiTable(msg)
-	fmt.Println(t)
+	log.Println(t)
 }
 
 func FuncOption() {
-	fmt.Println("https://github.com/pwzos/rediscmd")
-	CheckConf() //检查配置文件，如果配置文件不存在就需要初始化
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("工具发生致命错误！请通过https://github.com/pwzos/rediscmd向工具作者进行反馈")
+			log.Println(err)
+			log.Println("按任意键退出程序...")
+			InputReader.ReadString('\n')
+			os.Exit(1)
+		}
+	}()
+	log.Println("https://github.com/pwzos/rediscmd")
 	for {
 		FuncOptionMsg()
-		fmt.Println("请输出操作选项(回车结束输入)：")
+		log.Println("请输出操作选项(回车结束输入):")
 		option := ReadConsoleLineStr()
 		switch option {
 		case "1":
@@ -91,9 +104,15 @@ func FuncOption() {
 		case "6":
 			ReInitRedisConf_6()
 		case "7":
+			ChangeConfName_7()
+		case "8":
+			ReLoadAllCacheKeys_8()
+		case "9":
+			AddConf_9()
+		case "10":
 			os.Exit(1)
 		}
-		fmt.Println("按任意键开始下一轮操作...")
+		log.Println("按任意键开始下一轮操作...")
 		InputReader.ReadString('\n')
 		CallClear()
 	}
@@ -101,44 +120,50 @@ func FuncOption() {
 
 //加载数据
 func LoadDB_1() {
-	fmt.Println("是否需要展示所有数据库信息（y/n）:")
+	log.Println("是否需要展示所有数据库信息（y/n）:")
 	opt := ReadConsoleLineStr()
 	var dbInfoMap map[int]int
 	switch opt {
 	case "y":
+		log.Println("正在加载数据库信息，请稍候...")
 		dbInfoMap = LoadAllDBs(true, 0)
 	case "n":
-		fmt.Println("请输入您需要展示前多少个数据库信息（大于0的整数）:")
+		log.Println("请输入您需要展示前多少个数据库信息（大于0的整数）:")
 		countStr := ReadConsoleLineStr()
 		count, err := strconv.Atoi(countStr)
 		if err != nil || count <= 0 {
-			fmt.Println("您的输入的数量无法解析，请重来")
+			log.Println("您的输入的数量无法解析，请重来")
 			return
 		}
+		log.Println("正在加载数据库信息，请稍候...")
 		dbInfoMap = LoadAllDBs(false, count)
 	default:
-		fmt.Println("输入错误")
+		log.Println("输入错误")
 		return
 	}
 	for i := 0; i < len(dbInfoMap); i++ {
-		fmt.Printf("db(%d)=%d\r\n", i, dbInfoMap[i])
+		log.Printf("db(%d)=%d\r\n", i, dbInfoMap[i])
 	}
 }
 
 //加载数据库id和缓存key
 func DBIdAndKeyFromConsole(isLikeKey bool) (int, string, error) {
-	fmt.Println("请输入您要查询的数据库id（从0开始）:")
-	dbidStr := ReadConsoleLineStr()
+	msg := fmt.Sprintf("请输入您要操作的数据[0,%d) 和 模糊查询Key(*表示一个或者多个任意字符串)！使用一个空格分隔:", DbCount)
+	if !isLikeKey {
+		msg = fmt.Sprintf("请输入您要操作的数据[0,%d) 和 将要操作的缓存Key！使用一个空格分隔:", DbCount)
+	}
+	log.Println(msg)
+	dbIdAndKeyStr := ReadConsoleLineStr()
+	fEmptyStrIndex := strings.Index(dbIdAndKeyStr, " ")
+	if fEmptyStrIndex <= 0 {
+		return 0, "", errors.New("您输入的内容无法解析！请按照规则进行输入")
+	}
+	dbidStr := strings.Trim(dbIdAndKeyStr[0:fEmptyStrIndex], " ")
 	dbid, err := strconv.Atoi(dbidStr)
 	if err != nil {
 		return 0, "", errors.New("您输入的数据库id无法解析")
 	}
-	msg := "请输入模糊查询Key（*表示一个或者多个任意字符串）:"
-	if !isLikeKey {
-		msg = "请输入将要操作的缓存Key:"
-	}
-	fmt.Println(msg)
-	pattern := ReadConsoleLineStr()
+	pattern := strings.Trim(dbIdAndKeyStr[fEmptyStrIndex:], " ")
 	if pattern == "" {
 		return dbid, "", errors.New("您未输入任何缓存key")
 	}
@@ -149,12 +174,12 @@ func DBIdAndKeyFromConsole(isLikeKey bool) (int, string, error) {
 func LoadCacheKeys_2() {
 	dbid, pattern, err := DBIdAndKeyFromConsole(true)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 	keys, err := SearchKeys(dbid, pattern)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
@@ -163,7 +188,7 @@ func LoadCacheKeys_2() {
 		if item == "" {
 			continue
 		}
-		fmt.Println(item)
+		log.Println(item)
 	}
 }
 
@@ -171,53 +196,140 @@ func LoadCacheKeys_2() {
 func GetValue_3() {
 	dbid, pattern, err := DBIdAndKeyFromConsole(false)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	value, err := GetValue(pattern, dbid)
+	keys, err := SearchKeys(dbid, pattern)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	fmt.Println(value)
+	if len(keys) <= 0 {
+		log.Println("未查询找到任何缓存，您可以尝试刷新本地缓存key的操作再查询")
+		return
+	}
+	valueMsgChan := make(chan string, len(keys))
+	for _, key := range keys {
+		go func(itemKey string) {
+			value, err := GetValue(itemKey, dbid)
+			if err != nil {
+				valueMsgChan <- itemKey + "=" + err.Error()
+				return
+			}
+			valueMsgChan <- fmt.Sprintf("%s=%s", itemKey, value)
+		}(key)
+	}
+
+	//打印查询结果
+	for i := 0; i < len(keys); i++ {
+		log.Println(<-valueMsgChan)
+		log.Println()
+	}
 }
 
 //删除模糊key的值
 func DeleteKey_4() {
 	dbid, pattern, err := DBIdAndKeyFromConsole(false)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		return
+	}
+	if pattern == "*" {
+		log.Printf("根据您输入的模糊Key=%s此次操作将清空数据库dbid=%d中的所有缓存！请确认是否执行此操作(y/n):", pattern, dbid)
+		isSure := ReadConsoleLineStr()
+		if isSure == "y" {
+			log.Println("正在处理，请稍候...")
+			FlushDB(dbid) //清空数据库
+		}
 		return
 	}
 	keys, err := SearchKeys(dbid, pattern)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	for _, item := range keys {
-		DeleteKey(dbid, item)
+	allKeysCount := len(keys)
+	log.Printf("根据您输入的模糊Key=%s此次将批量删除%d条缓存数据，按任意键开始操作", pattern, allKeysCount)
+	InputReader.ReadString('\n')
+	deleteMsgChan := make(chan string, allKeysCount)
+	delCount := 1000
+	forCount := 0
+	for {
+		if delCount > len(keys) {
+			delCount = len(keys)
+		}
+		delKeys := keys[0:delCount]
+		go func(itemKey []string) {
+			start := time.Now()
+			DeleteKey(dbid, itemKey...) //一次性批量删除多个
+			deleteMsgChan <- fmt.Sprintf("%s 删除成功，耗时%d毫秒", itemKey, time.Since(start).Milliseconds())
+		}(delKeys)
+		forCount++
+		keys = keys[delCount:] //重新指定
+		if delCount >= len(keys) {
+			break
+		}
+	}
+	for i := 0; i < forCount; i++ {
+		log.Println(<-deleteMsgChan)
 	}
 }
 
 func SetValue_5() {
 	dbid, pattern, err := DBIdAndKeyFromConsole(false)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	fmt.Println("请输入您将要设置的值:")
+	log.Println("请输入您将要设置的值:")
 	valueStr := ReadConsoleLineStr()
 	SetValue(dbid, pattern, valueStr)
 }
 
+//重新配置当前设置当前配置文件的内容
 func ReInitRedisConf_6() {
+	confInited := false
 	for {
-		err := InitConf()
+		if !confInited {
+			err := InitConf()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+		}
+		confInited = true
+		err := initRedisConnectInfo() //因为重新配置了redis的连接信息，所以需要重新初始化redis连接信息
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			continue
 		}
 		break
 	}
+}
 
+//切换配置文件
+func ChangeConfName_7() {
+	for {
+		if err := initRedisConnectInfo(); err != nil {
+			log.Println(err)
+			continue
+		}
+		break
+	}
+}
+
+//触发加载缓存key
+func ReLoadAllCacheKeys_8() {
+	//遍历删除已加载的缓存key
+	CacheKeysMap.Range(func(k, v interface{}) bool {
+		CacheKeysMap.Delete(k)
+		return true
+	})
+	//触发加载缓存key
+	initLoadAllCacheKeys()
+}
+
+//添加配置文件
+func AddConf_9() {
+	createConfFile()
 }
--- src/util/redisutil.go
+++ src/util/redisutil.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -15,6 +16,16 @@ import (
 //redis连接池
 var redisPool *redis.Pool
 
+var DbCount int //数据库数量
+
+//redis缓存key信息
+var (
+	CacheKeysMap sync.Map
+)
+
+//获取连接的锁对象
+var CreateConnectLock sync.Mutex
+
 //数据库信息
 type DBInfo struct {
 	dbid, dbKeys int
@@ -22,10 +33,20 @@ type DBInfo struct {
 
 //初始化连接池信息
 func init() {
+	for {
+		if err := initRedisConnectInfo(); err != nil {
+			log.Println(err)
+			continue
+		}
+		break
+	}
+}
+
+//初始化redis连接信息
+func initRedisConnectInfo() error {
 	conf, err := GetConf()
 	if err != nil {
-		fmt.Println("配置文件加载失败")
-		return
+		return err
 	}
 	redisPool = &redis.Pool{
 		Dial: func() (conn redis.Conn, e error) {
@@ -33,42 +54,80 @@ func init() {
 			c.Do("AUTH", conf.Redis.Password) //认证
 			return c, err
 		},
-		MaxIdle:     3,
-		MaxActive:   100,
-		IdleTimeout: 240 * time.Second,
+		MaxIdle:     1,                     //连接池中最小空闲连接数
+		MaxActive:   conf.Redis.MaxConnect, //线程此的最大连接数
+		IdleTimeout: 60 * time.Second,      //30s之后，关闭多余的空闲连接
 	}
+	if err := initDBCount(); err != nil {
+		return err
+	} //初始化数据库数量
+	initLoadAllCacheKeys() //触发加载所有数据库中的缓存key信息
+	return nil
 }
 
-//获取一个连接
-func getConnection() redis.Conn {
+//获取连接要加锁
+func createConnection() redis.Conn {
+	CreateConnectLock.Lock()
 	for redisPool.ActiveCount() == redisPool.MaxActive {
 		time.Sleep(5 * time.Millisecond) //休眠一定时间，等待连接池空闲
 	}
-	return redisPool.Get()
+	conn := redisPool.Get()
+	CreateConnectLock.Unlock()
+	return conn
+}
+
+//加载所有数据库的缓存key到本地
+func initLoadAllCacheKeys() {
+	for dbid := 0; dbid < DbCount; dbid++ {
+		go func(dbIdItem int) {
+			conn := createConnection()
+			defer conn.Close()
+			conn.Do("select", dbIdItem)
+			keysRet, err := conn.Do("keys", "*")
+			if err != nil {
+				log.Println(err)
+			}
+			keysMap := make(map[string][]string)
+			if keysRet == nil { //当前数据库没有缓存key
+				CacheKeysMap.Store(dbIdItem, keysMap)
+				return
+			}
+
+			for _, item := range keysRet.([]interface{}) {
+				key := string(item.([]uint8))
+				if _, ok := keysMap[key]; !ok {
+					keysMap[strings.ToLower(key)] = []string{key}
+				} else {
+					keysMap[strings.ToLower(key)] = append(keysMap[strings.ToLower(key)], key)
+				}
+			}
+			CacheKeysMap.Store(dbIdItem, keysMap)
+		}(dbid)
+	}
 }
 
 //获取数据库的数量
-func DBCounts() int {
-	connection := getConnection()
-	defer getConnection().Close()
+func initDBCount() error {
+	connection := createConnection()
+	defer createConnection().Close()
 	connection.Do("select", "0") //选择指定数据库
 	ret, err := connection.Do("config", "get", "databases")
 	if err != nil {
-		log.Fatal("数据库列表加载错误，", err)
+		return err
 	}
 	ret1, ok := ret.([]interface{})
 	if !ok {
-		log.Fatal("类型转换失败")
+		return fmt.Errorf("获取数据库数量信息时发生类型转换错误")
 	}
-	dbCount, _ := strconv.Atoi(string(ret1[1].([]uint8)))
-	return dbCount
+	DbCount, _ = strconv.Atoi(string(ret1[1].([]uint8)))
+	return nil
 }
 
 //获取指定数据库中的keys数量
 func loadDBKeysCount(dbInfoChan chan DBInfo, dbid int) {
-	c := getConnection()
+	c := createConnection()
 	if c == nil {
-		fmt.Println("获取连接失败")
+		log.Println("获取连接失败")
 	}
 	defer c.Close()                    //关闭连接
 	c.Do("select", strconv.Itoa(dbid)) //选择指定数据库
@@ -82,7 +141,7 @@ func loadDBKeysCount(dbInfoChan chan DBInfo, dbid int) {
 
 //加载数据库列表
 func LoadAllDBs(isAll bool, count int) map[int]int {
-	dbCount := DBCounts() //获取数据库的数量
+	dbCount := DbCount //获取数据库的数量
 	if !isAll {
 		dbCount = count
 	}
@@ -114,28 +173,26 @@ func SearchKeys(dbid int, pattern string) ([]string, error) {
 	if pattern == "" {
 		pattern = "*"
 	}
-
-	conn := getConnection()
-	defer conn.Close()
-	conn.Do("select", dbid)
-	keysRet, err := conn.Do("keys", "*")
-	if err != nil {
-		return nil, errors.New("加载指定数据的所有key失败")
-	}
-	keysMap := make(map[string][]string)
-	for _, item := range keysRet.([]interface{}) {
-		key := string(item.([]uint8))
-		if _, ok := keysMap[key]; !ok {
-			keysMap[strings.ToLower(key)] = []string{key}
-		} else {
-			keysMap[strings.ToLower(key)] = append(keysMap[strings.ToLower(key)], key)
+	var keysMap map[string][]string
+	firstPrint := true
+	for {
+		if ret, ok := CacheKeysMap.Load(dbid); ok {
+			keysMap = ret.(map[string][]string)
+			break //缓存key已加载完成，可以继续后续操作
 		}
+		if !firstPrint {
+			log.Print(".")
+		} else {
+			firstPrint = false
+			log.Println("后台正在加载当前数据库中的缓存Key，请...")
+		}
+		time.Sleep(1 * time.Second) //休眠一秒
 	}
 
 	pattern = strings.ToLower(pattern)
 	pattern = strings.ReplaceAll(pattern, ".", "\\.")
 	pattern = strings.ReplaceAll(pattern, "*", ".*")
-	retKeys := make([]string, 10)
+	retKeys := make([]string, 0)
 	for k := range keysMap {
 		if ok, _ := regexp.Match(pattern, []byte(k)); ok {
 			retKeys = append(retKeys, keysMap[k]...)
@@ -149,7 +206,7 @@ func GetValue(key string, dbid int) (string, error) {
 	if key == "" {
 		return "", errors.New("key不能为空")
 	}
-	conn := getConnection()
+	conn := createConnection()
 	defer conn.Close()
 	conn.Do("select", dbid)
 	ret, err := conn.Do("get", key)
@@ -170,19 +227,26 @@ func SetValue(dbid int, key, value string) {
 	if key == "" {
 		return
 	}
-	conn := getConnection()
+	conn := createConnection()
 	defer conn.Close()
 	conn.Do("select", dbid)
 	conn.Do("set", key, value)
 }
 
 //删除缓存key
-func DeleteKey(dbid int, key string) {
-	if key == "" {
+func DeleteKey(dbid int, key ...string) {
+	if len(key) == 0 {
 		return
 	}
-	conn := getConnection()
+	conn := createConnection()
 	defer conn.Close()
 	conn.Do("select", dbid)
 	conn.Do("del", key)
+}
+
+func FlushDB(dbid int) {
+	conn := createConnection()
+	defer conn.Close()
+	conn.Do("select", dbid)
+	conn.Do("FLUSHDB")
 }
